973a939cdb61b07d5203a48d50eeeeca0b26f6ea
Mrzicokiller <zico_moerland@live.nl>
2018-05-20 13:38:30 +0200

    added basic routing

--- index.php
+++ index.php
@@ -6,6 +6,19 @@
  * Time: 12:41
  */
 
+$request_uri = explode("/", $_SERVER["REQUEST_URI"]);
+
+switch ($request_uri[0]) {
+    // Home page
+    case '/':
+        require 'pages/home.php';
+        break;
+    // Everything else
+    default:
+        header('HTTP/1.0 404 Not Found');
+        require 'pages/404.php';
+        break;
+}
 ?>
 
 
