430a9e8bc7942f8e3c4f03c6afe56bf7644c1e0c
Mrzicokiller <zico_moerland@live.nl>
2018-05-20 13:59:56 +0200

    removed routing

--- index.php
+++ index.php
@@ -5,20 +5,6 @@
  * Date: 20-5-2018
  * Time: 12:41
  */
-
-$request_uri = explode("/", $_SERVER["REQUEST_URI"]);
-
-switch ($request_uri[0]) {
-    // Home page
-    case '/':
-        require 'pages/home.php';
-        break;
-    // Everything else
-    default:
-        header('HTTP/1.0 404 Not Found');
-        require 'pages/404.php';
-        break;
-}
 ?>
 
 
