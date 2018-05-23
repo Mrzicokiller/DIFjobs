1134c31d846395f9bef1a9024cd16ab88151e889
Hijdra, L.J. (17043247) <17043247@student.hhs.nl>
2018-05-23 11:55:58 +0200

    added nav

--- pages/home.php
+++ pages/home.php
@@ -6,12 +6,19 @@
 </head>
 
 <body>
-<nav class="navbar navbar-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <?php
     include('../template/nav.php');
     ?>
 </nav>
-
+<!-- Bootstrap core JavaScript
+     ================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://code.jquery.com/jquery-1.12.4.js" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.16/js/jquery.dataTables.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.16/js/dataTables.bootstrap4.min.js"></script>
 </body>
 <?php
 include('../template/footer.php');
---
+++ template/nav.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lucas
+ * Date: 20-5-2018
+ * Time: 13:46
+ */
+?>
+
+<button class="navbar-toggler navbar-toggler-right hidden-lg-up" type="button" data-toggle="collapse"
+        data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false"
+        aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+</button>
+
+<div class="container">
+    <div class="col-xs-2 left">
+        <a class="navbar-brand" href="#">Briefjesboord</a>
+    </div>
+    <div class="col-xs-7 left">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Vacatures</a>
+            </li>
+        </ul>
+    </div>
+    <div class="col-xs-3 ml-0 pl-0 right">
+        <ul class="navbar-nav">
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">Login</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Registreren</a>
+            </li>
+        </ul>
+    </div>
+</div>
+
