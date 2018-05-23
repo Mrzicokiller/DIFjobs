42201fc4144c1724d0e7b8b7decc2866f00cb521
Mrzicokiller <zico_moerland@live.nl>
2018-05-23 13:45:37 +0200

    changed html tags

--- pages/home.php
+++ pages/home.php
@@ -1,3 +1,4 @@
+<html>
 <head>
     <?php
     include('../template/header.php');
--- pages/signup.php
+++ pages/signup.php
@@ -1,23 +1,29 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: mmoer
- * Date: 5/23/2018
- * Time: 11:45 AM
- */
-include("../template/header.php");
-?>
-<body>
-    <h1>Registreren</h1>
-<form>
-    <label for = "emailinput">Email</label>
-    <input type="text" name="email" id="emailinput"></input>
-    <br>
-    <label for = "passwordinput">Password</label>
-    <input type="text" name="password" id="passwordinput"></input>
+<html>
+    <head>
+        <?php
+        /**
+         * Created by PhpStorm.
+         * User: mmoer
+         * Date: 5/23/2018
+         * Time: 11:45 AM
+         */
+        include("../template/header.php");
+        ?>
+    </head>
 
-</form>
-</body>
-<?php
-include("../template/footer.php");
-?>
+    <body>
+        <h1>Registreren</h1>
+            <form>
+                <label for="emailinput">Email</label>
+                <input type="text" name="email" id="emailinput">
+                <br>
+                <label for="passwordinput">Password</label>
+                <input type="text" name="password" id="passwordinput">
+            </form>
+    </body>
+    <footer>
+        <?php
+        include("../template/footer.php");
+        ?>
+    </footer>
+</html>
