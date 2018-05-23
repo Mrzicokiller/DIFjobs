d993a931a804078089b9ddbd5365430062b1a1be
Hijdra, L.J. (17043247) <17043247@student.hhs.nl>
2018-05-23 11:37:41 +0200

    added home page

--- pages/home.php
+++ pages/home.php
@@ -6,20 +6,15 @@
 </head>
 
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">Briefjesboord</a>
-        <div class="navbar-nav">
-            <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-            <a class="nav-item nav-link" href="#">Features</a>
-            <a class="nav-item nav-link" href="#">Pricing</a>
-            <a class="nav-item nav-link " href="#">Disabled</a>
-            <span class="navbar-text">
-                <a class="nav-item nav-link" href="#">Pricing</a>
-                <a class="nav-item nav-link " href="#">Disabled</a>
-            </span>
-        </div>
+<nav class="navbar navbar-dark">
+    <?php
+    include('../template/nav.php');
+    ?>
 </nav>
+
+</body>
 <?php
 include('../template/footer.php');
 ?>
+
 </html>
--- template/header.php
+++ template/header.php
@@ -14,7 +14,7 @@
     <link href="../css/style.css" rel="stylesheet">
 
     <!-- Bootstrap core JavaScript
-================================================== -->
+      ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://code.jquery.com/jquery-1.12.4.js" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
