414c50f9dae13fada29120dd5f3c407aedfced12
mmoer <12021598@student.hhs.nl>
2018-05-23 12:06:32 +0200

    edited signup page

--- pages/signup.php
+++ pages/signup.php
@@ -5,3 +5,19 @@
  * Date: 5/23/2018
  * Time: 11:45 AM
  */
+include("../template/header.php");
+?>
+<body>
+    <h1>Registreren</h1>
+<form>
+    <label for = "emailinput">Email</label>
+    <input type="text" name="email" id="emailinput"></input>
+    <br>
+    <label for = "passwordinput">Password</label>
+    <input type="text" name="password" id="passwordinput"></input>
+
+</form>
+</body>
+<?php
+include("../template/footer.php");
+?>
--- template/header.php
+++ template/header.php
@@ -1,4 +1,5 @@
-
+<html>
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="project SE4">
@@ -21,3 +22,4 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.16/js/jquery.dataTables.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.16/js/dataTables.bootstrap4.min.js"></script>
+</head>
