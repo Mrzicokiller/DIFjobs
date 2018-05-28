697587574d7aa503bd6ca561a3f6212ee911ca0d
mmoer <12021598@student.hhs.nl>
2018-05-28 19:13:38 +0200

    eddited login page

--- POST/login.php
+++ POST/login.php
@@ -16,7 +16,16 @@ $loginpassword = mysqli_real_escape_string($mysqli, $loginwachtwoord);
 
 $loginpassword = password_hash($loginpassword, PASSWORD_BCRYPT);
 
+$checkEmailExist = mysqli_query("SELECT * FROM gebruiker WHERE email = '$loginemail'");
 
+if (mysqli_num_rows($checkEmailExist) > 0) {
+    $checkPassword = mysqli_query("SELECT wachtwoord FROM gebruiker WHERE email = '$loginemail'");
+    if($loginpassword === $checkPassword){
+        //start sessie etc?
+    }
+}else {
+    echo "dit emailadres is niet geregistreerd";
+}
 ?>
 
 
