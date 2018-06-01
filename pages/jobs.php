6234dccc92fb4099bcb710dfd44f6e468490f3e7
mmoer <12021598@student.hhs.nl>
2018-06-01 16:16:11 +0200

    laat jobs uit database zien

--- pages/jobs.php
+++ pages/jobs.php
@@ -2,7 +2,10 @@
 <html lang="nl">
 <head>
     <?php
-    include("../template/header.php")
+    include("../template/header.php");
+
+    include_once('../config.php');
+
     ?>
 </head>
 
@@ -14,178 +17,65 @@
 </nav>
 
 <div class="row mt-sm-4 ml-sm-2 mr-sm-2">
-    <div class="col-lg-12">
-        <h1>Vacatures</h1>
-        <hr/>
-    </div>
+	<div class="col-lg-12">
+		<h1>Vacatures</h1>
+		<hr/>
+	</div>
 </div>
 
 
 <div class="container-fluid">
 
-    <div class="row mt-lg-4 ml-sm-2 mr-sm-2">
-        <div class="col-lg-12">
-            <h2>Filter</h2>
-            <div class="row">
-                <div class="col-lg-2">
+	<div class="row mt-lg-4 ml-sm-2 mr-sm-2">
+		<div class="col-lg-12">
+			<h2>Filter</h2>
+			<div class="row">
+				<div class="col-lg-2">
 
-                    <label for="Labels">Selecteer een Label</label>
-                    <select class="form-control" id="Labels">
-                        <option>PHP ontwikkelaar</option>
-                        <option>CSS meester</option>
-                        <option>JS jester</option>
-                        <option>stage</option>
-                        <option>waarom zo veel</option>
-                    </select>
-                </div>
+					<label for="Labels">Selecteer een Label</label>
+					<select class="form-control" id="Labels">
+						<option>PHP ontwikkelaar</option>
+						<option>CSS meester</option>
+						<option>JS jester</option>
+						<option>stage</option>
+						<option>waarom zo veel</option>
+					</select>
+				</div>
 
-                <div class="col-lg-2">
-                    <label for="Search">Zoek een Label</label>
-                    <input type="text" id="Search" class="form-control" placeholder="Zoeken..">
-                </div>
-            </div>
-        </div>
-    </div>
+				<div class="col-lg-2">
+					<label for="Search">Zoek een Label</label>
+					<input type="text" id="Search" class="form-control" placeholder="Zoeken..">
+				</div>
+			</div>
+		</div>
+	</div>
 
-    <div class="row mt-lg-4 ml-sm-2 mr-sm-2">
-        <div class="col-lg-4">
-            <div class="card card-green">
-                <div class="card-heading">
-                    <div class="row px-sm-3 py-sm-3">
-                        <div class="col-lg-12">
-                            Me PC doet het niet
-                        </div>
-                    </div>
-                </div>
+    <?php
+    $haalJobQuery = "SELECT * FROM vacature";
+    $haalJobs = mysqli_query($mysqli, $haalJobQuery);
 
-                <div class="card-footer">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in sapien nulla. Proin rhoncus
-                    interdum nulla, a facilisis ex sollicitudin vitae. Etiam a elit non est bibendum ultrices. Phasellus
-                    egestas est non purus dapibus, semper euismod turpis aliquam. Nulla in purus varius, pretium erat
-                    sit amet, imperdiet purus. Nam consectetur, lacus sed bibendum fringilla, tortor purus consectetur
-                    leo, at ornare neque eros ac massa. Aliquam scelerisque consectetur venenatis. Integer pretium magna
-                    id augue maximus, vitae volutpat leo gravida. Nulla placerat ac odio quis condimentum. Proin eget
-                    odio placerat, bibendum eros quis, aliquam velit. Praesent odio tortor, pharetra nec augue eu,
-                    bibendum luctus sapien.
-                </div>
-            </div>
-        </div>
+    while ($rij = mysqli_fetch_array($haalJobs)) {
+        $jobTitel = $rij['Titel'];
+        $jobBeschrijving = $rij['Beschrijving'];
 
-        <div class="col-lg-4">
-            <div class="card card-green">
-                <div class="card-heading">
-                    <div class="row px-sm-3 py-sm-3">
-                        <div class="col-lg-12">
-                            #wegmetmac
-                        </div>
-                    </div>
-                </div>
+        ?>
 
-                <div class="card-footer">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in sapien nulla. Proin rhoncus
-                    interdum nulla, a facilisis ex sollicitudin vitae. Etiam a elit non est bibendum ultrices. Phasellus
-                    egestas est non purus dapibus, semper euismod turpis aliquam. Nulla in purus varius, pretium erat
-                    sit amet, imperdiet purus. Nam consectetur, lacus sed bibendum fringilla, tortor purus consectetur
-                    leo, at ornare neque eros ac massa. Aliquam scelerisque consectetur venenatis. Integer pretium magna
-                    id augue maximus, vitae volutpat leo gravida. Nulla placerat ac odio quis condimentum. Proin eget
-                    odio placerat, bibendum eros quis, aliquam velit. Praesent odio tortor, pharetra nec augue eu,
-                    bibendum luctus sapien.
-                </div>
-            </div>
-        </div>
+		<div class="row mt-lg-4 ml-sm-2 mr-sm-2">
+			<div class="col-lg-4">
+				<div class="card card-green">
+					<div class="card-heading">
+						<div class="row px-sm-3 py-sm-3">
+							<div class="col-lg-12">
+                                <?php echo "$jobTitel" ?>
+							</div>
+						</div>
+					</div>
 
-        <div class="col-lg-4">
-            <div class="card card-green">
-                <div class="card-heading">
-                    <div class="row px-sm-3 py-sm-3">
-                        <div class="col-lg-12">
-                            html & css
-                        </div>
-                    </div>
-                </div>
-
-                <div class="card-footer">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in sapien nulla. Proin rhoncus
-                    interdum nulla, a facilisis ex sollicitudin vitae. Etiam a elit non est bibendum ultrices. Phasellus
-                    egestas est non purus dapibus, semper euismod turpis aliquam. Nulla in purus varius, pretium erat
-                    sit amet, imperdiet purus. Nam consectetur, lacus sed bibendum fringilla, tortor purus consectetur
-                    leo, at ornare neque eros ac massa. Aliquam scelerisque consectetur venenatis. Integer pretium magna
-                    id augue maximus, vitae volutpat leo gravida. Nulla placerat ac odio quis condimentum. Proin eget
-                    odio placerat, bibendum eros quis, aliquam velit. Praesent odio tortor, pharetra nec augue eu,
-                    bibendum luctus sapien.
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="row mt-lg-4 ml-sm-2 mr-sm-2">
-        <div class="col-lg-4">
-            <div class="card card-red">
-                <div class="card-heading">
-                    <div class="row px-sm-3 py-sm-3">
-                        <div class="col-lg-12">
-                            js developer
-                        </div>
-                    </div>
-                </div>
-
-                <div class="card-footer">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in sapien nulla. Proin rhoncus
-                    interdum nulla, a facilisis ex sollicitudin vitae. Etiam a elit non est bibendum ultrices. Phasellus
-                    egestas est non purus dapibus, semper euismod turpis aliquam. Nulla in purus varius, pretium erat
-                    sit amet, imperdiet purus. Nam consectetur, lacus sed bibendum fringilla, tortor purus consectetur
-                    leo, at ornare neque eros ac massa. Aliquam scelerisque consectetur venenatis. Integer pretium magna
-                    id augue maximus, vitae volutpat leo gravida. Nulla placerat ac odio quis condimentum. Proin eget
-                    odio placerat, bibendum eros quis, aliquam velit. Praesent odio tortor, pharetra nec augue eu,
-                    bibendum luctus sapien.
-                </div>
-            </div>
-        </div>
-
-        <div class="col-lg-4">
-            <div class="card card-red">
-                <div class="card-heading">
-                    <div class="row px-sm-3 py-sm-3">
-                        <div class="col-lg-12">
-                            vuilnis man
-                        </div>
-                    </div>
-                </div>
-
-                <div class="card-footer">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in sapien nulla. Proin rhoncus
-                    interdum nulla, a facilisis ex sollicitudin vitae. Etiam a elit non est bibendum ultrices. Phasellus
-                    egestas est non purus dapibus, semper euismod turpis aliquam. Nulla in purus varius, pretium erat
-                    sit amet, imperdiet purus. Nam consectetur, lacus sed bibendum fringilla, tortor purus consectetur
-                    leo, at ornare neque eros ac massa. Aliquam scelerisque consectetur venenatis. Integer pretium magna
-                    id augue maximus, vitae volutpat leo gravida. Nulla placerat ac odio quis condimentum. Proin eget
-                    odio placerat, bibendum eros quis, aliquam velit. Praesent odio tortor, pharetra nec augue eu,
-                    bibendum luctus sapien.
-                </div>
-            </div>
-        </div>
-
-        <div class="col-lg-4">
-            <div class="card card-red">
-                <div class="card-heading">
-                    <div class="row px-sm-3 py-sm-3">
-                        <div class="col-lg-12">
-                            PHP developer
-                        </div>
-                    </div>
-                </div>
-                <div class="card-footer">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in sapien nulla. Proin rhoncus
-                    interdum nulla, a facilisis ex sollicitudin vitae. Etiam a elit non est bibendum ultrices. Phasellus
-                    egestas est non purus dapibus, semper euismod turpis aliquam. Nulla in purus varius, pretium erat
-                    sit amet, imperdiet purus. Nam consectetur, lacus sed bibendum fringilla, tortor purus consectetur
-                    leo, at ornare neque eros ac massa. Aliquam scelerisque consectetur venenatis. Integer pretium magna
-                    id augue maximus, vitae volutpat leo gravida. Nulla placerat ac odio quis condimentum. Proin eget
-                    odio placerat, bibendum eros quis, aliquam velit. Praesent odio tortor, pharetra nec augue eu,
-                    bibendum luctus sapien.
-                </div>
-
-            </div>
-        </div>
-    </div>
+					<div class="card-footer">
+                        <?php echo "$jobBeschrijving" ?>
+					</div>
+				</div>
+			</div>
+		</div>
+    <?php } ?>
 </div>
