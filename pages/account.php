c0a2d89ed4047daa98f19485c73032215cf3f756
dereluc11 <lucas-hijdra@hotmail.com>
2018-06-01 13:54:18 +0200

    added account

--- pages/account.php
+++ pages/account.php
@@ -8,7 +8,7 @@
 
 include_once('../config.php');
 
-$mysqli->query('SELECT');
+//$mysqli->query('SELECT');
 
 ?>
 
@@ -40,10 +40,10 @@ $mysqli->query('SELECT');
                     <div class="card-header">
                         <ul class="nav nav-tabs card-header-tabs">
                             <li class="nav-item">
-                                <a class="nav-link" id="dataTab" href="#">Gegevens</a>
+                                <a class="nav-link active" id="dataTab" href="#">Gegevens</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link active" id="jobsTab" href="#">Vacatures</a>
+                                <a class="nav-link" id="jobsTab" href="#">Vacatures</a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" id="responseTab" href="#">Reacties</a>
@@ -51,17 +51,118 @@ $mysqli->query('SELECT');
                         </ul>
                     </div>
                 </div>
-                <div class="card-body" id="dataBody" hidden>
+                <div class="card-body" id="dataBody">
                     <h1 class="card-title">Gegevens</h1>
+
+                    <h3>Naam:</h3>
+                    <form name="changeName" action="../POST/updateUserData.php" method="post">
+
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="naam" placeholder="Naam">
+                        </div>
+                        <br/>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                    <hr/>
+
+                    <h3>wachtwoord veranderen:</h3>
+                    <form name="changePassword" action="../POST/updateUserData.php" method="post">
+
+                        <div class="form-group">
+                            <label for="password">Wachtwoord*</label>
+                            <input type="password" class="form-control" id="password" placeholder="Wachtwoord" required>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="confirmPassword">Herhaal Wachtwoord*</label>
+                            <input type="password" class="form-control" id="confirmPassword" placeholder="Wachtwoord" required>
+                        </div>
+                        <br/>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+
+                    <!-- forms als gebruiker student is -->
+                    <h3>Specialisatie:</h3>
+                    <form name="specialisatie" action="../POST/updateUserData.php?F=student" method="post">
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="specialisatie" placeholder="C# developer">
+                        </div>
+                        <br/>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                    <!-- form als gebruiker bedrijf is -->
+                    <!-- form als gebruiker particulier is -->
 
                 </div>
 
-                <div class="card-body" id="jobsBody">
+                <div class="card-body" id="jobsBody" hidden>
+
+                    <!-- alleen bedrijf-->
                     <h1 class="card-title">Vacature</h1>
+
+                    <table class="table table-striped table-dark">
+                        <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Titel</th>
+                            <th scope="col">Datum</th>
+                            <th scope="col">Functie</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>php man</td>
+                            <td>12-01-2017</td>
+                            <td>php</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>php man</td>
+                            <td>12-01-2017</td>
+                            <td>php</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>php man</td>
+                            <td>12-01-2017</td>
+                            <td>php</td>
+                        </tr>
+                        </tbody>
+                    </table>
                 </div>
 
                 <div class="card-body" id="responseBody" hidden>
+
+                    <!-- alleen studenten-->
                     <h1 class="card-title">reactie</h1>
+
+                    <table class="table table-striped table-dark">
+                        <thead>
+                        <tr>
+                            <th scope="col">VacatureID</th>
+                            <th scope="col">datum</th>
+                            <th scope="col">bericht</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>12-01-2017</td>
+                            <td>php</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>12-01-2017</td>
+                            <td>php</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>12-01-2017</td>
+                            <td>php</td>
+                        </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
