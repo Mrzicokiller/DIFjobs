06d09671869e188f0bf5765fbb44300c7630e305
Mrzicokiller <zico_moerland@live.nl>
2018-06-13 12:27:46 +0200

    password reset begin gemaakt

---
+++ POST/password_reset.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zico_
+ * Date: 13-6-2018
+ * Time: 12:08
+ */
+
+include_once('../config.php');
+include_once ('../assets/php/code_generator.php');
+
+if(!isset($_GET['email']))
+{
+    $email = $_POST['email'];
+    $email = mysqli_real_escape_string($mysqli, $email);
+
+
+    $verifiedCode = code_generator(10);
+
+//variable voor de email
+    $subject = "Wachtwoord resetten";
+    $message =
+        '<html>
+        <head>
+            <title>Email bevestigen</title>
+        </head>
+        <body>
+            <p>Druk op de onderstaande link om je email de te rifiëren.</p><br/>
+            <a href="localhost/password_reset.php?email=' . $email . '&code=' . $verifiedCode . '">Email verifiëren</a>       
+        </body>
+     </html>
+    ';
+
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+    $headers .= 'From: <briefjesboord@test.nl>' . "\r\n";
+
+    if(filter_var($email, FILTER_VALIDATE_EMAIL))
+    {
+        if($mysqli->query("UPDATE gebruiker SET verifiedCode = '$verifiedCode' WHERE email = '$email'"))
+        {
+            echo "Er is een email gestuurd met een link";
+        }
+        else
+        {
+            echo "Er is iets fout gegaan. Neem contact op met de beheerder";
+        }
+    }
+}
+else
+{
+    ?>
+
+    <!DOCTYPE html>
+<html lang="nl">
+<head>
+    <?php
+    session_start();
+    include("../template/header.php")
+    ?>
+</head>
+
+<body>
+<nav class="navbar navbar-expand-lg bg-dark">
+    <?php
+    include('../template/nav.php')
+    ?>
+</nav>
+
+<div class="container-fluid">
+    <div class="row mt-sm-4 ml-sm-2 mr-sm-2">
+        <div class="col-lg-12">
+            <h1>Wachtwoord Resetten</h1>
+            <hr/>
+        </div>
+    </div>
+
+    <div class="row ml-sm-2 mr-sm-2">
+        <div class="col-lg-12">
+            <form name="passwordResetForm" onsubmit="resetPassword();">
+                <label for="emailInput">Vul hier je email adres in.</label>
+                <input id="emailInput" type="email" class="form-control">
+                <button class="btn btn-primary" type="submit">Submit</button>
+            </form>
+            <h3 id="reactie"></h3>
+        </div>
+    </div>
+
+</div>
+
+</body>
+</html>
+
+<?php
+}
+?>
+
---
+++ pages/wachtwoord_reset.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <?php
+    session_start();
+    include("../template/header.php")
+    ?>
+</head>
+
+<body>
+<nav class="navbar navbar-expand-lg bg-dark">
+    <?php
+    include('../template/nav.php')
+    ?>
+</nav>
+
+<div class="container-fluid">
+    <div class="row mt-sm-4 ml-sm-2 mr-sm-2">
+        <div class="col-lg-12">
+            <h1>Wachtwoord Resetten</h1>
+            <hr/>
+        </div>
+    </div>
+
+    <div class="row ml-sm-2 mr-sm-2">
+        <div class="col-lg-12">
+            <form name="passwordResetForm" onsubmit="resetPassword();">
+                <label for="emailInput">Vul hier je email adres in.</label>
+                <input id="emailInput" type="email" class="form-control">
+                <button class="btn btn-primary" type="submit">Submit</button>
+            </form>
+            <h3 id="reactie"></h3>
+        </div>
+    </div>
+
+</div>
+<script>
+    function resetPassword()
+    {
+        var email = $('#emailInput').val();
+
+        $.post("../POST/email_check.php", {
+                email: $("#email").val()
+            },
+            function (result)
+            {
+                if (result == 1) {
+                    $('#reactie').val('Deze email bestaat niet in onze database.');
+                }
+                else {
+                    $.post('../POST/password_reset',{
+                            email: email
+                        },
+                        function (result2) {
+                            $('#reactie').val(result2);
+                        });
+                }
+
+            });
+    }
+</script>
+</body>
+</html>
+
