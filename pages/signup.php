cc8f3fa02cc022a2d173a9ba065d9481583de7a8
Mrzicokiller <zico_moerland@live.nl>
2018-06-15 12:36:40 +0200

    cleaned

--- pages/signup.php
+++ pages/signup.php
@@ -18,6 +18,13 @@
             include('../template/nav.php');
             ?>
         </nav>
+    <div class="container-fluid">
+        <div class="row mt-sm-4 ml-sm-2 mr-sm-2">
+            <div class="col-lg-12">
+                <h1>Registreren</h1>
+                <hr/>
+            </div>
+        </div>
     <div class="row mx-auto">
         <div class="col-sm-6">
             <!--Registratie form-->
@@ -114,9 +121,4 @@
     }
 </script>
 </body>
-<footer>
-    <?php
-    include("../template/footer.php");
-    ?>
-</footer>
 </html>
