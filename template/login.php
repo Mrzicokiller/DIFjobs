8678948c48835a522e3464d48b9a2675e0a60b4a
dereluc11 <lucas-hijdra@hotmail.com>
2018-06-15 13:10:33 +0200

    trying to fix login

--- template/login.php
+++ template/login.php
@@ -10,14 +10,14 @@
                         <label for="usrname"><span class="glyphicon glyphicon-user"></span> Email</label>
                         <input type="email" class="form-control" id="loginEmail" name="loginEmail"
                                placeholder="voer email in" required>
-                        <small id="loginEmailError" class="redLetters" hidden>Dit emailadres is niet bekend.
-                        </small>
+                        <small id="loginEmailError" class="redLetters">Dit emailadres is niet bekend.</small>
                     </div>
+
                     <div class="form-group">
                         <label for="psw"><span class="glyphicon glyphicon-eye-open"></span> Wachtwoord</label>
                         <input type="password" class="form-control" id="loginWachtwoord" name="loginWachtwoord"
                                placeholder="voer wachtwoord in" autocomplete="off" required>
-                        <small id="loginPasswordError" class="redLetters" hidden>Dit wachtwoord is incorrect</small>
+                        <small id="loginPasswordError" class="redLetters">Dit wachtwoord is incorrect</small>
                     </div>
                     <button onclick="submitForm();" type="button" class="btn btn-primary">Inloggen</button>
                 </form>
@@ -48,26 +48,6 @@
 
         if (checkEmailDone !== true || checkPasswordDone !== true) {
 
-            //bestaat de email al
-            if ($("#loginEmail").val().length > 0) {
-                $.post("../POST/email_check.php", {
-                        email: $("#loginEmail").val()
-                    },
-                    function (result) {
-                        if (result == 1) {
-                            checkEmailDone = true;
-                            $("#loginEmailError").hide();
-                        }
-                        else {
-                            $("#loginEmailError").show();
-                            checkEmailDone = false;
-                        }
-
-                    });
-            }else{
-                $("#loginEmailError").show();
-            }
-
 
             //wachtwoorden vergelijken
             if ($("#loginWachtwoord").val().length > 0) {
@@ -78,10 +58,33 @@
                 checkPasswordDone = false;
             }
 
+
+            //bestaat de email al
+                $.post("../POST/email_check.php", {
+                        email: $("#loginEmail").val()
+                    },
+                    function (result) {
+                        if (result == 1) {
+                            $("#loginEmailError").hide();
+                            checkEmailDone = true;
+                            alert(checkEmailDone + "," + checkPasswordDone);
+
+                            if (checkEmailDone === true && checkPasswordDone === true) {
+                                $("#loginform").submit();
+                            }
+                        }
+                        else {
+                            $("#loginEmailError").show();
+                            checkEmailDone = false;
+                        }
+
+                    });
+
+
+
+
         }
 
-        if (checkEmailDone === true && checkPasswordDone === true) {
-            $("#loginform").submit();
-        }
+
     }
 </script>
