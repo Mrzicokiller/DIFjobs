4d2d35adcf469dccf7912ed5fa4493dfb7b33403
dereluc11 <lucas-hijdra@hotmail.com>
2018-06-15 15:59:25 +0200

    added success msg for login

--- pages/home.php
+++ pages/home.php
@@ -32,60 +32,72 @@ if (isset($_GET['postsuccess']) && $_GET['postsuccess'] == 2) {
 ?>
 
 <div class="container-fluid">
-	<div class="row mt-sm-4 ml-sm-2 mr-sm-2">
-		<div class="col-lg-12">
-			<h1>Home</h1>
-			<hr/>
-		</div>
-	</div>
+    <div class="row mt-sm-4 ml-sm-2 mr-sm-2">
+        <div class="col-lg-12">
+            <h1>Home</h1>
+            <hr/>
+        </div>
+    </div>
 
-	<div class="row ml-sm-2 mr-sm-2">
-		<div class="col-lg-12">
-			<div class="card card-green">
-				<div class="card-heading">
-					<div class="row px-lg-3 py-lg-3">
-						<div class="col-sm-3">
-							<i class="fa fa-briefcase fa-5x"></i>
-						</div>
-						<div class="col-sm-9 text-right">
-							<div class="huge">
+    <div class="row ml-sm-2 mr-sm-2">
+        <div class="col-lg-12 mt-3">
+            <?php
+            if (isset($_GET['login']) && $_GET['login'] == true) {
+                echo "<div class='alert alert-success'>Je bent succesvol ingelogd.</div>";
+            } elseif (isset($_GET['login']) && $_GET['login'] == false) {
+                echo "<div class='alert alert-danger'>Er is iets mis gegaan tijdens het in loggen, probeer het opnieuw of neem contact op met de beheerder.</div>";
+            }
+            ?>
+        </div>
+    </div>
+    <div class="row ml-sm-2 mr-sm-2">
+        <div class="col-lg-12 mt-3">
+            <div class="card card-green">
+                <div class="card-heading">
+                    <div class="row px-lg-3 py-lg-3">
+                        <div class="col-sm-3">
+                            <i class="fa fa-briefcase fa-5x"></i>
+                        </div>
+                        <div class="col-sm-9 text-right">
+                            <div class="huge">
                                 <?php
-								//toont aantal vacatures in de db
-								echo $vacCount->num_rows; ?>
-							</div>
-							<div>
-								Vacatures!
-							</div>
-						</div>
-					</div>
-				</div>
-				<a href="jobs.php">
-					<div class="card-footer">
-						<span class="pull-left">Toon direct</span>
-						<span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-						<div class="clearfix"></div>
-					</div>
-				</a>
-			</div>
-		</div>
-	</div>
-    <?php
-        if (isset($_GET['loginError']) && $_GET['loginError'] == true) {
-            echo "<div class='alert alert-danger col-lg-10 ml-4 mr-auto mt-2'>Er is iets mis gegaan tijdens het in loggen, probeer het opnieuw of neem contact op met de beheerder.</div>";
-        }
-    ?>
-	<div class="row ml-sm-2 mr-sm-2 mt-lg-5">
-		<div class="col-lg-12">
-			<h2>Welkom op Dif Jobs!</h2>
-			<p>
-				Dif Jobs is de website die bedrijven, particulieren en studenten bij elkaar brengt zodat er productieve samenwerkingen onstaan waar iedereen
-				baat bij hebben. Bedrijven en particulieren kunnen na het registreren van een account advertenties plaatsen met daarin vraagstukken opdrachten
-				die door studenten kunnen worden uitgevoerd. De advertenties worden weergegeven op de jobs pagina. Studenten kunnen na het registreren van een
-				account op advertenties reageren door een bericht te sturen naar het bedrijf dat de advertentie heeft geplaatst. Dit doe je door
-				simpelweg op de reageerknop onder een advertentie te klikken.
-			</p>
-		</div>
-	</div>
+                                //toont aantal vacatures in de db
+                                echo $vacCount->num_rows; ?>
+                            </div>
+                            <div>
+                                Vacatures!
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <a href="jobs.php">
+                    <div class="card-footer">
+                        <span class="pull-left">Toon direct</span>
+                        <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                        <div class="clearfix"></div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="row ml-sm-2 mr-sm-2 mt-lg-4">
+    <div class="col-lg-12">
+        <h2>Welkom op Dif Jobs!</h2>
+        <p>
+            Dif Jobs is de website die bedrijven, particulieren en studenten bij elkaar brengt zodat er productieve
+            samenwerkingen onstaan waar iedereen
+            baat bij hebben. Bedrijven en particulieren kunnen na het registreren van een account advertenties
+            plaatsen met daarin vraagstukken opdrachten
+            die door studenten kunnen worden uitgevoerd. De advertenties worden weergegeven op de jobs pagina.
+            Studenten kunnen na het registreren van een
+            account op advertenties reageren door een bericht te sturen naar het bedrijf dat de advertentie heeft
+            geplaatst. Dit doe je door
+            simpelweg op de reageerknop onder een advertentie te klikken.
+        </p>
+    </div>
+</div>
 </div>
 
 
