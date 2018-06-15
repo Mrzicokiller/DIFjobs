500a4038f2fd7012c135fa24f6180e40c6debaa2
mmoer <12021598@student.hhs.nl>
2018-06-15 14:34:13 +0200

    accepteert

--- pages/signup.php
+++ pages/signup.php
@@ -53,7 +53,7 @@
 
                 <div class="form-check">
                     <input type="checkbox" class="form-check-input" name="avgCheck" id="avgCheck" required>
-                    <label class="form-check-label" for="avgCheck">Accepteerd u dat wij uw gegevens mogen opslaan.(Deze zullen wij niet delen met een derde.)</label>
+                    <label class="form-check-label" for="avgCheck">Accepteert u dat wij uw gegevens mogen opslaan.(Deze zullen wij niet delen met een derde.)</label>
 
                 </div>
                 <button onclick="submitForm()" type="button" class="btn btn-primary">Registreren</button>
